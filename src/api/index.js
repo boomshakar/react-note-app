6d770778b470092eb914f828f5456d04dd345bac
Oluwafemi Ṣosanya <Hefehmzy@gmail.com>
2021-09-13 09:13:50 +0100

    update

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
-// export const url = "http://localhost:5050/api";
-export const url = "https://boom-note-app.herokuapp.com/";
+export const url = "http://localhost:5050/api";
+// export const url = "https://boom-note-app.herokuapp.com";
 
 export const setHeaders = () => {
   const headers = {
